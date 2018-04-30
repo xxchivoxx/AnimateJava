d975da0a3b875d5dadeea8c9a7eb03940967ed29
carlos <jocaguerraro@ittepic.edu.mx>
2018-04-30 17:28:09 -0600

    Initial commit

---
+++ app/src/main/java/com/example/android/java/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.android.java;
+
+import android.animation.Animator;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.faceIcon);
+    }
+
+    public void onButtonClick(View v) {
+        imageView.animate()
+                .alpha(0)
+                .scaleX(2)
+                .scaleY(2)
+                .scaleXBy(1)
+                .scaleYBy(1)
+                .setDuration(3000)
+                .setListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                        Log.i("Animation", "onAnimationStart");
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        imageView.animate()
+                                .alpha(1)
+                                .translationX(200)
+                                .translationY(100)
+                                .translationY(-300)
+                                .rotation(50)
+                                .setListener(new Animator.AnimatorListener() {
+                                    @Override
+                                    public void onAnimationStart(Animator animator) {
+
+                                    }
+
+                                    @Override
+                                    public void onAnimationEnd(Animator animator) {
+                                        imageView.animate()
+                                                .alpha(1)
+                                                .translationX(-400)
+                                                .rotation(-600)
+                                                .setListener(new Animator.AnimatorListener() {
+                                            @Override
+                                            public void onAnimationStart(Animator animator) {
+
+                                            }
+
+                                            @Override
+                                            public void onAnimationEnd(Animator animator) {
+                                                imageView.animate()
+                                                        .alpha(0)
+                                                        .translationX(350)
+                                                        .translationY(-500)
+                                                        .scaleX(2)
+                                                        .scaleY(2)
+                                                        .rotation(-600);
+                                            }
+
+                                            @Override
+                                            public void onAnimationCancel(Animator animator) {
+
+                                            }
+
+                                            @Override
+                                            public void onAnimationRepeat(Animator animator) {
+
+                                            }
+                                        });
+                                    }
+
+                                    @Override
+                                    public void onAnimationCancel(Animator animator) {
+
+                                    }
+
+                                    @Override
+                                    public void onAnimationRepeat(Animator animator) {
+
+                                    }
+                                });
+                        Log.i("Animation", "onAnimationEnd");
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                });
+    }
+
+}
